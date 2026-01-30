30764e3ed5b829db6bf0192be4104e9c6216ac0c
Siddharth <siddhu0902.c@gmail.com>
2026-01-30 18:11:05 +0530

    Reversing string through method reverse1(),Creating cricket.txt

--- Java_1/src/J2.java
+++ Java_1/src/J2.java
@@ -2,13 +2,21 @@ public class J2 {
     public static void main(String[] args){
         System.out.println("Mathematical tables");
         String s1 = "";
-        for(int j=3;j<9;j++){
+        for(int j=3;j<6;j++){
             for(int i=1;i<11;i++){
                 s1 = s1+j+"*"+i+"="+(j*i)+"\n";
             }
             s1=s1+"\n";
         }
 
+        for(int i=1;i<11;i++){
+            s1 = s1+4+"*"+i+"="+(4*i)+"\n";
+        }
+        s1=s1+"\n";
+        for(int i=1;i<11;i++){
+            s1 = s1+5+"*"+i+"="+(5*i)+"\n";
+        }
+        s1=s1+"\n";
         System.out.println(s1);
     }
 }
--- Java_1/src/J3.java
+++ Java_1/src/J3.java
@@ -1,12 +1,18 @@
 public class J3 {
-    public static void main(String[] args){
-        System.out.println("Reversing a string");
-        String s1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static String reverse1(String s1){
         String s2 = "";
         int len1=s1.length();
         for(int i=1;i<len1+1;i++){
             s2 = s2+s1.substring(len1-i,len1+1-i);
         }
-        System.out.println(s2);
+        return s2;
+    }
+    public static void main(String[] args){
+        String name1="Siddharth";
+        String name2="Chandrashekar";
+        String name1_rev = reverse1(name1);
+        String name2_rev = reverse1(name2);
+        System.out.println(name1+"-"+name1_rev);
+        System.out.println(name2+"-"+name2_rev);
     }
 }
