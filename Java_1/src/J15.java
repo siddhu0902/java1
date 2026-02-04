79c142c6813b871eb6f850d3261916c5e1980084
Siddharth <siddhu0902.c@gmail.com>
2026-02-04 19:04:25 +0530

    Gold medal: Toppers in all 5 subjects

--- Java_1/src/J15.java
+++ Java_1/src/J15.java
@@ -12,9 +12,15 @@ public class J15 {
 
         List<Integer> engMarks=new ArrayList<>();
         List<Integer> matMarks=new ArrayList<>();
+        List<Integer> phyMarks=new ArrayList<>();
+        List<Integer> cheMarks=new ArrayList<>();
+        List<Integer> bioMarks=new ArrayList<>();
 
         List<String> engToppers=new ArrayList<>();
         List<String> matToppers=new ArrayList<>();
+        List<String> phyToppers=new ArrayList<>();
+        List<String> cheToppers=new ArrayList<>();
+        List<String> bioToppers=new ArrayList<>();
 
         String s1="";
         String[] arr1;
@@ -22,6 +28,9 @@ public class J15 {
 
         int engMax=0;
         int matMax=0;
+        int phyMax=0;
+        int cheMax=0;
+        int bioMax=0;
 
         int len1=0;
         while(sc1.hasNext()){
@@ -36,20 +45,47 @@ public class J15 {
             arr2=arr1[4].split(":");
             subjects.add(arr2[0]);
             matMarks.add(Integer.parseInt(arr2[1]));
+
+            arr2=arr1[5].split(":");
+            subjects.add(arr2[0]);
+            phyMarks.add(Integer.parseInt(arr2[1]));
+
+            arr2=arr1[6].split(":");
+            subjects.add(arr2[0]);
+            cheMarks.add(Integer.parseInt(arr2[1]));
+
+            arr2=arr1[7].split(":");
+            subjects.add(arr2[0]);
+            bioMarks.add(Integer.parseInt(arr2[1]));
         }
         engMax= Collections.max(engMarks);
         matMax= Collections.max(matMarks);
+        phyMax= Collections.max(phyMarks);
+        cheMax= Collections.max(cheMarks);
+        bioMax= Collections.max(bioMarks);
 
         len1=engMarks.size();
         for (int i=0;i<len1;i++){
             if (engMarks.get(i)==engMax){
                 engToppers.add(names.get(i));
             }
-            if ((matMarks.get(i)==matMax)){
+            if (matMarks.get(i)==matMax){
                 matToppers.add(names.get(i));
+            }
+            if (phyMarks.get(i)==phyMax){
+                phyToppers.add(names.get(i));
+            }
+            if (cheMarks.get(i)==cheMax){
+                cheToppers.add(names.get(i));
+            }
+            if (bioMarks.get(i)==bioMax){
+                bioToppers.add(names.get(i));
             }
         }
         System.out.println(engToppers+" are the toppers in "+subjects.get(0)+" with marks: "+engMax);
         System.out.println(matToppers+" are the toppers in "+subjects.get(1)+" with marks: "+matMax);
+        System.out.println(phyToppers+" are the toppers in "+subjects.get(2)+" with marks: "+phyMax);
+        System.out.println(cheToppers+" are the toppers in "+subjects.get(3)+" with marks: "+cheMax);
+        System.out.println(bioToppers+" are the toppers in "+subjects.get(4)+" with marks: "+bioMax);
     }
 }
