2b03e4024d087a93b3e15a0a3295a996d9c502fd
Siddharth <siddhu0902.c@gmail.com>
2026-02-04 18:52:09 +0530

    Gold medal: Toppers in Maths

--- Java_1/src/J15.java
+++ Java_1/src/J15.java
@@ -8,30 +8,48 @@ public class J15 {
         File f1=new File("Students.txt");
         Scanner sc1 = new Scanner(f1);
         List<String> names=new ArrayList<>();
+        List<String> subjects=new ArrayList<>();
+
         List<Integer> engMarks=new ArrayList<>();
+        List<Integer> matMarks=new ArrayList<>();
+
         List<String> engToppers=new ArrayList<>();
-        List<Integer> mathMarks=new ArrayList<>();
-        List<String> mathToppers=new ArrayList<>();
+        List<String> matToppers=new ArrayList<>();
+
         String s1="";
         String[] arr1;
         String[] arr2;
-        int mathMax=0;
+
         int engMax=0;
+        int matMax=0;
+
         int len1=0;
         while(sc1.hasNext()){
             s1=sc1.nextLine();
             arr1 =s1.split(",");
             names.add(arr1[0]);
+
             arr2=arr1[3].split(":");
+            subjects.add(arr2[0]);
             engMarks.add(Integer.parseInt(arr2[1]));
+
+            arr2=arr1[4].split(":");
+            subjects.add(arr2[0]);
+            matMarks.add(Integer.parseInt(arr2[1]));
         }
         engMax= Collections.max(engMarks);
+        matMax= Collections.max(matMarks);
+
         len1=engMarks.size();
         for (int i=0;i<len1;i++){
             if (engMarks.get(i)==engMax){
                 engToppers.add(names.get(i));
             }
+            if ((matMarks.get(i)==matMax)){
+                matToppers.add(names.get(i));
+            }
         }
-        System.out.println(engToppers+" are the toppers in English with marks: "+engMax);
+        System.out.println(engToppers+" are the toppers in "+subjects.get(0)+" with marks: "+engMax);
+        System.out.println(matToppers+" are the toppers in "+subjects.get(1)+" with marks: "+matMax);
     }
 }
