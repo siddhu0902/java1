6d9506c080290312ed145ee288feb6e86352394f
Siddharth <siddhu0902.c@gmail.com>
2026-02-05 20:07:28 +0530

    Diamond left upper

---
+++ Java_1/src/J20.java
@@ -0,0 +1,17 @@
+public class J20 {
+    public static void main(String[] args){
+        String word = "ABCDEFGHIJK";
+        int s=word.length();
+        String part1="";
+        String part2="";
+        int len1=word.length();
+        for (int j=1;j<len1+1;j++){
+            part1="";
+            for (int i=0;i<len1-j;i++){
+                part1=part1+" ";
+            }
+            part2=word.substring(0,j);
+            System.out.println(part1+part2);
+        }
+    }
+}
